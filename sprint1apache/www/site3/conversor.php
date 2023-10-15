b56e1b93e46610d16fa9ccf81f719c3d320ac959
NoePedreiraC <npedreirac@fpcoruna.afundacion.org>
2023-10-15 21:39:10 +0200

    Ejercicio5 PHP

---
+++ sprint1apache/www/site3/conversor.php
@@ -0,0 +1,18 @@
+<html>
+   <body>
+	<h1>Conversor de longitudes</h1
+	<p> Combierte de la unidad especificada a metros </p>
+	<p>
+	<?php
+	if  ($_GET["unidad"] == "pulgadas") {
+	$v_pulgadas = $_GET["cantidad"];
+	$v_metros = $v_pulgadas * 0,0254;
+	echo $v_pulgadas."pulgada(s) = ".$v_metros." metro(s)";
+	} else {
+	  echo "Unidad no soportada";
+	}
+	<?php
+	</p>
+   >/body>
+</html>
+
