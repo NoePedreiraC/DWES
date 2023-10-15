0e06f39b961dfbb2ff5c5c2484694921525af74c
NoePedreiraC <npedreirac@fpcoruna.afundacion.org>
2023-10-15 20:53:48 +0200

    Ejercicio3 PHP

---
+++ sprint1apache/www/site3/example3.php
@@ -0,0 +1,20 @@
+<html>
+   <body>
+	<h1>Jubilacion</h1
+	<?php
+	$edad=$_GET["edad"];
+
+	function edad-en_10_annos($edad) {
+
+	return $edad +10;
+	}
+	if (edad_en_10_annos($edad) >65){
+
+	echo "En 10 años tendras edad de jubilación";
+	} else {
+	echo "Disfruta de tu tiempo!!";
+	}
+	?>
+   >/body>
+</html>
+
