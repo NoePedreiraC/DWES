609c4fb5a0ad6cbdb9698142a9403f0f2cdb1784
NoePedreiraC <npedreirac@fpcoruna.afundacion.org>
2023-10-15 20:37:40 +0200

    Ejercicio2 PHP

---
+++ sprint1apache/www/site3/example2.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<h1>Pagina de bienvenidas</h1>
+		<?php
+		 function dar_bienvenida($nombre) {
+		   echo "¡Bienvenido/a, " $nombre . "!";
+		}
+
+		dar_bienvenida("Apu");
+		?>
+	</body>
+</html>
