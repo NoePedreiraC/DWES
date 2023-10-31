79af7534d69494ab08184ad1dcf5711d7930be76
Noé Pedreira <145000797+NoePedreiraC@users.noreply.github.com>
2023-10-31 12:56:26 +0100

    Ejercicio2 python

--- sprint2python/adivinanza.py
+++ sprint2python/adivinanza.py
@@ -1,21 +1,57 @@
-# Definimos la adivinanza y las opciones
-adivinanza = "¿Qué animal camina en cuatro patas por la mañana, en dos patas al mediodía y en tres patas por la noche?"
-opciones = {
-    'a': 'Un perro',
-    'b': 'Un gato',
-    'c': 'Un elefante'
+ # Definimos las adivinanzas y las opciones en un diccionario
+adivinanzas = {
+    1: {
+        'pregunta': "¿Qué animal camina en cuatro patas por la mañana, en dos patas al mediodía y en tres patas por la noche?",
+        'opciones': {
+            'a': 'Un perro',
+            'b': 'Un gato',
+            'c': 'Un elefante'
+        },
+        'respuesta_correcta': 'b'
+    },
+    2: {
+        'pregunta': "Tiene dientes y no puede comer, ¿qué es?",
+        'opciones': {
+            'a': 'Un peine',
+            'b': 'Un cuchillo',
+            'c': 'Un tenedor'
+        },
+        'respuesta_correcta': 'a'
+    },
+    3: {
+        'pregunta': "Es redonda como la luna, pero siempre llena de estrellas, ¿qué es?",
+        'opciones': {
+            'a': 'Una ventana',
+            'b': 'Una puerta',
+            'c': 'Una mesa'
+        },
+        'respuesta_correcta': 'a'
+    }
 }
 
-# Imprimimos la adivinanza y las opciones
-print(adivinanza)
-for opcion, descripcion in opciones.items():
-    print(f'({opcion}) {descripcion}')
+# Inicializamos la puntuación del usuario
+puntuacion = 0
 
-# Solicitamos la respuesta al usuario
-respuesta = input("Elije la opción correcta (a, b o c): ").lower()
+# Iteramos a través de las adivinanzas
+for num_adivinanza, adivinanza_info in adivinanzas.items():
+    pregunta = adivinanza_info['pregunta']
+    opciones = adivinanza_info['opciones']
 
-# Verificamos si la respuesta es correcta
-if respuesta == 'b':
-    print("¡Correcto! La respuesta es un gato.")
-else:
-    print("Respuesta incorrecta. La respuesta correcta es (b) Un gato.")
+    # Imprimimos la adivinanza y las opciones
+    print(f"\nAdivinanza {num_adivinanza}:\n{pregunta}")
+    for opcion, descripcion in opciones.items():
+        print(f'({opcion}) {descripcion}')
+
+    # Solicitamos la respuesta al usuario
+    respuesta = input("Elige la opción correcta (a, b o c): ").lower()
+
+    # Verificamos si la respuesta es correcta
+    if respuesta == adivinanza_info['respuesta_correcta']:
+        print("¡Correcto! Has ganado 10 puntos.")
+        puntuacion += 10
+    else:
+        print(f"Respuesta incorrecta. La respuesta correcta es ({adivinanza_info['respuesta_correcta']}). Has perdido 5 puntos.")
+        puntuacion -= 5
+
+# Imprimimos la puntuación final del usuario
+print(f"\nPuntuación final: {puntuacion} puntos")
